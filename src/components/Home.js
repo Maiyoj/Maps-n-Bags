524453fae8d1b6004fb20f222df51a0d361bab3d
Maiyoj <valentinemaiyo1995@gmail.com>
2022-07-25 12:34:41 +0300

    add background iage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,12 @@
 function Home(){
     return(
-        <p>Hello  world</p>
+        <div className="home">
+            <div className="content">
+                <h1>Life is an Adventure</h1>
+                <p>Where Do You Want To Start Your Next Adventure?</p>
+            </div>
+
+        </div>
     )
 
 }
