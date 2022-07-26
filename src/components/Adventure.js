382445875e8de82300634a0277330902813ba449
Maiyoj <valentinemaiyo1995@gmail.com>
2022-07-26 09:19:17 +0300

    update styling on blog page

--- src/components/Adventure.js
+++ src/components/Adventure.js
@@ -1,18 +1,17 @@
 function Adventure(){
   return (
     <>
+    <h1 clas >About Us</h1>
     <div className="about">
-    <img src={process.env.PUBLIC_URL + '/images/kijiji pictures-2299-2.jpg'} alt="Valentine"></img>
-    <p>Hey, I'm Ciara! I'm a 20 something who is passionate about living an adventurous life and helping others to do the same. After working in corporate Human Resources for nearly 2 years, I recently quit my job to travel full time and document my experience along the way. I created this blog to share my travel photos, tips, thoughts, and stories with you!
+    <img src={process.env.PUBLIC_URL + '/images/val.jpg'} alt="Valentine"></img>
+    <p>Hey, I'm Valentine! I'm a 20 something who is passionate about living an adventurous life and helping others to do the same. After working in corporate Human Resources for nearly 2 years, I recently quit my job to travel full time and document my experience along the way. I created this blog to share my travel photos, tips, thoughts, and stories with you!
 
 I traveled out of the country for the first time at 20 years old.  After teaching basic accounting concepts to indigenous(native) communities in Panama through a study abroad program, I fell in love with travel. I loved the exchange of cultures, daring adventures & walking into the unknown. I then went on to spend my 21st birthday in India and my 22nd birthday in South Africa through study abroad programs.
 
 Since graduating college, I continued to make travel a priority - both locally and globally. I found myself extremely unfulfilled and unsatisfied with my professional life. I also wanted to see more of the world and continue having extraordinary experiences. While working, I managed to save as much money as possible for the inevitable. After nearly 2 years, I made the leap and quit my job for full time travel. This was probably the most difficult decision I've ever had to make during my adult life - Do I leave behind what's 'right', 'normal', and 'stable'? Or do something  that I've always dreamed of doing? Fear of the unknown is REAL, yall!
 
 I can now happily say that leaving my job for an extraordinary life was the best decision I've ever made. I am now doing two things I always dreamed of - traveling the world & inspiring others to do the same. So if you're reading this, please know that it means the world to me! I am honored that you've decided to follow along with me on this journey! I hope that I am able to leave you with something that helps you, entertains you, or simply makes you smile. 
-
 KEEP GOING
-
 Make yourself at home and say hi so that i can get to know you as well!</p>
     </div>
     </>
