ee0565655f0ce8146e5ccabe480fe096007705b4
Maiyoj <valentinemaiyo1995@gmail.com>
2022-07-26 10:27:55 +0300

    add comment  div and style

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,11 +3,15 @@ function Comments(){
 
      <div className="commentForm">
          <h1>Leave a Comment</h1>
+         <div className="displayComment">
+         <p>comment: High</p>
+         <p>Author: Hello valentine</p>
+         </div>
          <form className="updateForm">
          <textarea placeholder="Comment"></textarea><br/>
          <input placeholder="name"></input>
          </form>
-         <p>comment1</p>
+    
      </div>
      )
 }
