2f5cdb1070effd38d4442a0f437280bf0fa0d9cd
Maiyoj <valentinemaiyo1995@gmail.com>
2022-07-29 16:44:24 +0300

    [update] about image size

--- src/components/Adventure.js
+++ src/components/Adventure.js
@@ -11,7 +11,7 @@ function Adventure(){
       Since graduating college, I continued to make travel a priority - both locally and globally. I found myself extremely unfulfilled and unsatisfied with my professional life. I also wanted to see more of the world and continue having extraordinary experiences. While working, I managed to save as much money as possible for the inevitable. After nearly 2 years, I made the leap and quit my job for full time travel. This was probably the most difficult decision I've ever had to make during my adult life - Do I leave behind what's 'right', 'normal', and 'stable'? Or do something  that I've always dreamed of doing? Fear of the unknown is REAL, yall!
       I can now happily say that leaving my job for an extraordinary life was the best decision I've ever made. I am now doing two things I always dreamed of - traveling the world & inspiring others to do the same. So if you're reading this, please know that it means the world to me! I am honored that you've decided to follow along with me on this journey! I hope that I am able to leave you with something that helps you, entertains you, or simply makes you smile. 
       KEEP GOINg Make yourself at home and say hi so that i can get to know you as well!</p>
-      <img src={process.env.PUBLIC_URL + '/images/girl.jpg'} alt="Valentine"></img>
+      <img src={process.env.PUBLIC_URL + '/images/girl.jpg'} alt="Valentine" className='image2'></img>
       <p>Cruise with me</p>
     </div>
     
